fd16c17a428536288868cc0ddfd484c03e244847
zyddnys <zyddnys@outlook.com>
2020-02-20 23:11:16 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,236 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/go-ego/gse"
+)
+
+type segRequest struct {
+	Text string
+}
+
+type segResponse struct {
+	Words []string
+}
+
+var (
+	seger gse.Segmenter
+)
+
+func main() {
+	fmt.Println("[+] TextSeg start")
+	seger.LoadDict("chs.txt,cht.txt,jpn.txt,touhou.txt")
+	fmt.Println("[+] Done loading dict")
+	http.HandleFunc("/s/", segTextSearch)
+	http.HandleFunc("/i/", segTextIndex)
+	fmt.Println("[+] Serving ...")
+	http.ListenAndServe("0.0.0.0:5005", nil)
+}
+
+func unique(elements []string) []string {
+	// Use map to record duplicates as we find them.
+	encountered := map[string]bool{}
+	result := []string{}
+
+	for v := range elements {
+		if encountered[elements[v]] == true {
+			// Do not add duplicate.
+		} else {
+			// Record this element as an encountered element.
+			encountered[elements[v]] = true
+			// Append to result slice.
+			if len(elements[v]) > 2 {
+				result = append(result, elements[v])
+			}
+		}
+	}
+	// Return the new slice.
+	return result
+}
+
+func segTextSearch(w http.ResponseWriter, r *http.Request) {
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	requestBodyStr := strings.ToLower(string(requestBody))
+	fmt.Println("seg search")
+	segResult := make([]string, 0, len(requestBody))
+	splitText := strings.FieldsFunc(requestBodyStr, split)
+	for i := 0; i < len(splitText); i++ {
+		tmpSegResult := seger.Cut(splitText[i], true)
+		segResult = append(segResult, tmpSegResult...)
+	}
+	allWords := unique(segResult)
+	resp := segResponse{allWords}
+	js, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func segTextIndex(w http.ResponseWriter, r *http.Request) {
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	requestBodyStr := strings.ToLower(string(requestBody))
+	fmt.Println("seg index")
+	segResult := make([]string, 0, len(requestBody))
+	splitText := strings.FieldsFunc(requestBodyStr, split)
+	for i := 0; i < len(splitText); i++ {
+		tmpSegResult := seger.CutSearch(splitText[i], true)
+		segResult = append(segResult, tmpSegResult...)
+	}
+
+	allWords := unique(segResult)
+	resp := segResponse{allWords}
+	js, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func split(r rune) bool {
+	return r == ':' ||
+		r == '.' ||
+		r == '\n' ||
+		r == '\r' ||
+		r == '[' ||
+		r == ']' ||
+		r == ' ' ||
+		r == '\t' ||
+		r == '\v' ||
+		r == '\f' ||
+		r == '{' ||
+		r == '}' ||
+		r == '-' ||
+		r == '_' ||
+		r == '=' ||
+		r == '+' ||
+		r == '`' ||
+		r == '~' ||
+		r == '!' ||
+		r == '@' ||
+		r == '#' ||
+		r == '$' ||
+		r == '%' ||
+		r == '^' ||
+		r == '&' ||
+		r == '*' ||
+		r == '(' ||
+		r == ')' ||
+		r == ';' ||
+		r == '\'' ||
+		r == '"' ||
+		r == ',' ||
+		r == '<' ||
+		r == '>' ||
+		r == '/' ||
+		r == '?' ||
+		r == '\\' ||
+		r == '|' ||
+		r == '－' ||
+		r == '＞' ||
+		r == '＜' ||
+		r == '。' ||
+		r == '，' ||
+		r == '《' ||
+		r == '》' ||
+		r == '【' ||
+		r == '】' ||
+		r == '　' ||
+		r == '？' ||
+		r == '！' ||
+		r == '￥' ||
+		r == '…' ||
+		r == '（' ||
+		r == '）' ||
+		r == '、' ||
+		r == '：' ||
+		r == '；' ||
+		r == '·' ||
+		r == '「' ||
+		r == '」' ||
+		r == '『' ||
+		r == '』' ||
+		r == '〔' ||
+		r == '〕' ||
+		r == '［' ||
+		r == '］' ||
+		r == '｛' ||
+		r == '｝' ||
+		r == '｟' ||
+		r == '｠' ||
+		r == '〉' ||
+		r == '〈' ||
+		r == '〖' ||
+		r == '〗' ||
+		r == '〘' ||
+		r == '〙' ||
+		r == '〚' ||
+		r == '〛' ||
+		r == '゠' ||
+		r == '＝' ||
+		r == '‥' ||
+		r == '※' ||
+		r == '＊' ||
+		r == '〽' ||
+		r == '〓' ||
+		r == '〇' ||
+		r == '＂' ||
+		r == '“' ||
+		r == '”' ||
+		r == '‘' ||
+		r == '’' ||
+		r == '＃' ||
+		r == '＄' ||
+		r == '％' ||
+		r == '＆' ||
+		r == '＇' ||
+		r == '＋' ||
+		r == '．' ||
+		r == '／' ||
+		r == '＠' ||
+		r == '＼' ||
+		r == '＾' ||
+		r == '＿' ||
+		r == '｀' ||
+		r == '｜' ||
+		r == '～' ||
+		r == '｡' ||
+		r == '｢' ||
+		r == '｣' ||
+		r == '､' ||
+		r == '･' ||
+		r == 'ｰ' ||
+		r == 'ﾟ' ||
+		r == '￠' ||
+		r == '￡' ||
+		r == '￢' ||
+		r == '￣' ||
+		r == '￤' ||
+		r == '￨' ||
+		r == '￩' ||
+		r == '￪' ||
+		r == '￫' ||
+		r == '￬' ||
+		r == '￭' ||
+		r == '￮' ||
+		r == '・'
+}
