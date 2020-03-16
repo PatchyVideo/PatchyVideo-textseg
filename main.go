40af39ef185141f2f225b2c56987c3d70689d091
zyddnys <zyddnys@outlook.com>
2020-03-15 20:38:01 -0400

    update

--- main.go
+++ main.go
@@ -24,10 +24,12 @@ var (
 
 func main() {
 	fmt.Println("[+] TextSeg start")
-	seger.LoadDict("chs.txt,cht.txt,jpn.txt,touhou.txt")
+	seger.LoadDict("touhou.txt,touhou2.txt,chs.txt,cht.txt,jpn.txt")
 	fmt.Println("[+] Done loading dict")
 	http.HandleFunc("/s/", segTextSearch)
 	http.HandleFunc("/i/", segTextIndex)
+	http.HandleFunc("/d/", segTextDisplay)
+	http.HandleFunc("/t/", segTextTouhou)
 	fmt.Println("[+] Serving ...")
 	http.ListenAndServe("0.0.0.0:5005", nil)
 }
@@ -49,6 +51,90 @@ func unique(elements []string) []string {
 	}
 	// Return the new slice.
 	return result
+}
+
+func segTextDisplay(w http.ResponseWriter, r *http.Request) {
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	requestBodyStr := strings.ToLower(string(requestBody))
+
+	//splitText := strings.FieldsFunc(requestBodyStr, split)
+	//for i := 0; i < len(splitText); i++ {
+	//tb := []byte(splitText[i])
+	tb := []byte(requestBodyStr)
+	segments := seger.Segment(tb)
+
+	// Handle word segmentation results
+	// Support for normal mode and search mode two participle,
+	// see the comments in the code ToString function.
+	// The search mode is mainly used to provide search engines
+	// with as many keywords as possible
+	//fmt.Println(gse.ToString(segments, true))
+	ret := [][]string{}
+	for i := 0; i < len(segments); i++ {
+		seg := segments[i]
+		//wordType := seg.Token().Pos()
+		//if wordType == "TH名詞" {
+		ele := []string{seg.Token().Text(), seg.Token().Pos()}
+		ret = append(ret, ele)
+		//fmt.Printf("%s=>%s\n", )
+		//}
+	}
+	//}
+
+	js, err := json.Marshal(ret)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func segTextTouhou(w http.ResponseWriter, r *http.Request) {
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	requestBodyStr := strings.ToLower(string(requestBody))
+
+	//splitText := strings.FieldsFunc(requestBodyStr, split)
+	//for i := 0; i < len(splitText); i++ {
+	//tb := []byte(splitText[i])
+	tb := []byte(requestBodyStr)
+	segments := seger.Segment(tb)
+
+	// Handle word segmentation results
+	// Support for normal mode and search mode two participle,
+	// see the comments in the code ToString function.
+	// The search mode is mainly used to provide search engines
+	// with as many keywords as possible
+	//fmt.Println(gse.ToString(segments, true))
+	ret := []string{}
+	for i := 0; i < len(segments); i++ {
+		seg := segments[i]
+		wordType := seg.Token().Pos()
+		if wordType == "TH名詞" {
+			ret = append(ret, seg.Token().Text())
+		}
+	}
+
+	uniqueRet := unique(ret)
+	//}
+
+	js, err := json.Marshal(uniqueRet)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
 func segTextSearch(w http.ResponseWriter, r *http.Request) {
